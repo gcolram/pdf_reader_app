ae72d396b2da1069596f300bbc26826bc7066014
gcolram <gcollazosr5@gmail.com>
2024-07-26 20:47:54 +0200

    Remove example.pdf and test_api.py from version control

--- test_api.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'https://pdf-reader-app.onrender.com/read_pdf'  # URL de tu aplicación en Render
-files = {'file': open('example.pdf', 'rb')}  # Asegúrate de proporcionar la ruta correcta al archivo PDF
-response = requests.post(url, files=files)
-
-print(response.json())
